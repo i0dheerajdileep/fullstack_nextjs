715cc59b90e748a1ac82c19c0dfa4a69277b8ccf
Dheeraj <i.dheerajdileep@gmail.com>
2023-06-23 21:00:45 +0530

    folder structure

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <Link href='/'>Next App</Link>
+    </div>
+  )
+}
+
+export default Navbar
