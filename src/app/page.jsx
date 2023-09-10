b6df21da1d5d0abad20c3a3782d86e8ee4e5af43
Dheeraj <i.dheerajdileep@gmail.com>
2023-09-11 00:17:09 +0530

    hero done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
+import Hero  from '../assets/hero.png'
 
-function page() {
+function Home() {
   return (
-    <div>
-      hey
+    <div className='flex items-center gap-[100px]'>
+      <div className='flex flex-col gap-[50px]'>
+      <h1 className="text-7xl font-bold" style={{ fontSize: '72px', background: 'linear-gradient(to bottom, #194c33, #bbb)', WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>Better design <br/>for your digital <br/>products</h1>
+        <p className=' text-lg'>Turn your ideas into reality.We bring together <br/>the teams from the global tech industry</p>
+        <button className='cursor-pointer p-[20px] bg-[#53c28b] max-w-max text-md font-bold text-white  border-none rounded-md'>See our works</button>
+      </div>
+      <div>
+        <Image src={Hero} alt="" className='w-full h-[500px] object-cover'/>
+      </div>
     </div>
   )
 }
 
-export default page
+export default Home
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
+import fb from '@/assets/fb.png'
 
 function Footer() {
   return (
-    <div>
+    <div className='h-[50px] items-center justify-between flex'>
       <div>Copyrights reserved by Dheeraj dileep</div>
-      <div></div>
+      <div className='h-[18px] w-[18px] relative cursor-pointer'>
+        <Image src={fb} fill={true}/>
+      </div>
     </div>
   )
 }
